45beea0ffb5bca2aa744691632c6f077d41bfd62
GreatTheGreatestDragon <greetjon@mail.ru>
2024-09-06 19:27:58 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+Homeworks_Counts = 12
+Hours_Counts = 1.5
+Name_Of_Curse = 'Python'
+Time_One_Lesson = Hours_Counts / Homeworks_Counts
+print('Курс: ', Name_Of_Curse,', ''всего задач:',Homeworks_Counts,', ''среднее время выполнения ',Time_One_Lesson)
